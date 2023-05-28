10b1ef3eafe73f12c1a944bdbabd3712e0e17821
Alexander <14233825+Straffern@users.noreply.github.com>
2023-05-28 15:20:20 +0200

    Created test for BsTree.new

--- test/bs_tree_test.exs
+++ test/bs_tree_test.exs
@@ -6,4 +6,23 @@ defmodule BsTreeTest do
     assert BsTree.hello() == :world
   end
 
+  describe "new/2" do
+    test "create new empty binary search tree, with comparator" do
+      assert %BsTree{root: nil} = BsTree.new([], fn a, b -> a <= b end)
+    end
+
+    test "create tree with a root node when given one element" do
+      assert %BsTree{root: %BsTree.Node{value: 1, left: nil, right: nil}} = BsTree.new([1])
+    end
+
+    test "create tree with a root node and a left node when given two elements" do
+      assert %BsTree{root: %BsTree.Node{value: 2, left: %BsTree.Node{value: 1, left: nil, right: nil}, right: nil}} = BsTree.new([2, 1])
+    end
+
+    test "create tree with a root node and a right node when given two elements" do
+      assert %BsTree{root: %BsTree.Node{value: 1, left: nil, right: %BsTree.Node{value: 2, left: nil, right: nil}}} = BsTree.new([1, 2])
+    end
+  end
+
+
 end
