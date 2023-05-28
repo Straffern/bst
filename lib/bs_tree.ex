79a31a500e95d096043c4ee4221cee101c51bc12
Alexander <14233825+Straffern@users.noreply.github.com>
2023-05-28 10:25:10 +0200

    Init project

---
+++ lib/bs_tree.ex
@@ -0,0 +1,18 @@
+defmodule BsTree do
+  @moduledoc """
+  Documentation for `BsTree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BsTree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bs_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule BsTreeTest do
+  use ExUnit.Case
+  doctest BsTree
+
+  test "greets the world" do
+    assert BsTree.hello() == :world
+  end
+end
